3a56529cfd065f8e289638c5ba9f387d938c6241
PJSeon <heabaple@gmail.com>
2022-01-18 13:02:35 +0900

    20220118

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,5 +1,9 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요~
-document.querySelector(".result").addEventListener("click", function(){
-    alert("ddd")
+document.querySelectorAll(".result").forEach(function(el){
+    el.addEventListener("click", function(){
+        alert("dd")
+    })
 });
+//toggle써야됨
+
 // document.querySelector(".result").addEventListener("click", () => {});
