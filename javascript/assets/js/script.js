428b1e15e899586689c257ca013e243e58dbafaf
PJSeon <heabaple@gmail.com>
2022-01-19 10:17:03 +0900

    20220119

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,10 +1,8 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요~
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-       document.querySelectorAll(".result").forEach(function(el){
-           el.classList.toggle("click");
-       })
-    })
+        el.classList.toggle("active");
+    });
 });
 //toggle써야됨
 
